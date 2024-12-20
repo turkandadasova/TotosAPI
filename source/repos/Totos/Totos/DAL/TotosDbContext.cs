2015253938cb34cac2bd7b904ce3eef3244de94e
Your Name <you@example.com>
2024-12-20 11:37:43 +0400

    version 1

---
+++ source/repos/Totos/Totos/DAL/TotosDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Totos.Entities;
+
+namespace Totos.DAL
+{
+    public class TotosDbContext : DbContext
+    {
+        public TotosDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Language> Languages { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Language>(b =>
+            {
+
+                b.HasKey(x => x.Code);
+                b.Property(x => x.Code).IsFixedLength(true).HasMaxLength(2);
+                b.HasIndex(x => x.Name).IsUnique();
+                b.Property(x => x.Name).IsRequired().HasMaxLength(32);
+                b.Property(x => x.Icon).IsRequired().HasMaxLength(128);
+
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ source/repos/Totos/Totos/Entities/Language.cs
@@ -0,0 +1,9 @@
+﻿namespace Totos.Entities
+{
+    public class Language
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+    }
+}
